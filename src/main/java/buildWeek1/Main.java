8c945b895903e77dfc0013cf69318ee1c8a545da
ManuelSenatore <106093444+ManuelSenatore@users.noreply.github.com>
2022-10-21 15:53:57 +0200

    Initial commit

---
+++ src/main/java/buildWeek1/Main.java
@@ -0,0 +1,15 @@
+package buildWeek1;
+
+import models.dao.ResellerDAO;
+import models.display.PrincipalDisplay;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//System.out.println(ResellerDAO.getById(2L));
+		
+		PrincipalDisplay.open();
+		
+	}
+}
